bf5c462ea543cc7cdb3c4ce42eae85e5f565ed30
Vellisca <vellisca.alejandro@gmail.com>
2023-12-04 13:34:50 +0100

    Ultimos cambios EJ2 added with while loop added

--- novocales.js
+++ novocales.js
@@ -1,15 +1,18 @@
-const sc = require("prompt-sync")({ sigint: true });
-let texto =sc("Escribe una cadena de texto, yo diré la suma de los caracteres que escribas dentro --> ")
+let texto =sc("Escribe una cadena de texto, yo diré la suma de los caracteres que escribas dentro, escribe cancelar para salir --> ")
 let textomin= texto.toLowerCase();
 let num='0123456789'
 let suma=0;
 //console.log(textomin)
-for (let n = 0; n < textomin.length; n++) {
-    for (let i = 0; i < num.length; i++) {
-        if (textomin.charAt(n)==num.charAt(i)) {
-            suma=Number(num.charAt(i))+suma;
+while (textomin!='cancelar') {
+    for (let n = 0; n < textomin.length; n++) {
+        for (let i = 0; i < num.length; i++) {
+            if (textomin.charAt(n)==num.charAt(i)) {
+                suma=Number(num.charAt(i))+suma;
+            }
         }
     }
+    texto =sc("Escribe una cadena de texto, yo diré la suma de los caracteres que escribas dentro, escribe cancelar para salir --> ")
+    textomin= texto.toLowerCase();
 }
 if (suma==0) {
     console.log("No había números en la cadena de texto ");
